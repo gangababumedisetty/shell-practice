ee8664a73ccbb7e3500968aec908308fad727085
gangababu M <gangababumedisetty.com>
2026-02-03 11:23:29 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh , I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learinig Ramesh?"
+echo "Rmaesh:: I am learining shell. Waht about you?"
