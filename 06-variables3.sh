35e970da68d009b85402ba8e674e81f4707eae5d
gangababu M <gangababumedisetty.com>
2026-02-03 12:55:09 +0530

    actions

---
+++ 06-variables3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# interview qns how can you exciute and take the output in shell script 
+# ans : we can put the command into $ and take the output from varailble
+
+TIMESTAMP=$(date)
+
+echo "Script executed at : $TIMESTAMP"
