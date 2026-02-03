66e777744989d53f5deee42d8ff6c0f7b6fd58e7
gangababu M <gangababumedisetty.com>
2026-02-03 14:04:28 +0530

    actions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,13 +4,12 @@ NUMBER=$1
 
 # -gt -> greater than
 # -lt -> less than
-# -eq -> equal 
+# -eq -> equal
 # -ne -> not equal
-if [[$NUMBER -gt 20]]; then
-    echo "Given number: $NUMBER is greather than 20"
-elif [[ $NUMBER -eq 20 ]]; then
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
     echo "Given number: $NUMBER is equal to 20"
 else
     echo "Given number: $NUMBER is less than 20"
 fi
-
