39fdf621cc1fea2a071be5d7ab0d965ee04dc9b4
gangababu M <gangababumedisetty.com>
2026-02-03 13:07:16 +0530

    actions

--- 06-variables3.sh
+++ 06-variables3.sh
@@ -3,6 +3,13 @@
 # interview qns how can you exciute and take the output in shell script 
 # ans : we can put the command into $ and take the output from varailble
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
-echo "Script executed at : $TIMESTAMP"
+echo "Script executed at : $START_TIME"
+
+sleep 12
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script excted in: $TOTAL_TIME seconds"
